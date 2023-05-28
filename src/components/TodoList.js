c35bb9834e64b630858c28e5c9b6df42d3f74912
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-05-28 09:30:47 +0900

    Learn Recoil with Live Walkthrough

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,21 @@
-import { todoListState } from '../recoil_state';
+import { filteredTodoListState, todoListState } from '../recoil_state';
 
 import TodoItemCreator from './TodoItemCreator';
 
 import TodoItem from './TodoItem';
 import { useRecoilValue } from 'recoil';
+import TodoListStats from './TodoListStats';
+import TodoListFilters from './TodoListFilters';
 
 const TodoList = () => {
-  const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
   return (
     <>
-      <TodoItemCreator />
+      <TodoListStats />
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <TodoItemCreator />
+        <TodoListFilters />
+      </div>
       {todoList.map((todoItem) => (
         <TodoItem item={todoItem} key={todoItem.id} />
       ))}
---
+++ src/components/TodoListFilters.js
@@ -0,0 +1,24 @@
+import { useRecoilState } from 'recoil';
+import { todoListFilterState } from '../recoil_state';
+
+function TodoListFilters() {
+  const [filter, setFilter] = useRecoilState(todoListFilterState);
+
+  const updateFilter = ({ target: { value } }) => {
+    setFilter(value);
+  };
+
+  return (
+    <>
+      Filter:
+      <select value={filter} onChange={updateFilter}>
+        <option value="Show All">All</option>
+        <option value="Show Completed">Completed</option>
+        <option value="Show Uncompleted">Uncompleted</option>
+      </select>
+      <p></p>
+    </>
+  );
+}
+
+export default TodoListFilters;
